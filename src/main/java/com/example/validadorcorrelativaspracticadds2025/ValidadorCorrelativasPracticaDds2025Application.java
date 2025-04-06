fc69ed64bb728b8e438fd04a0767a39d88dce43b
SebaR1 <romerosebastian781@gmail.com>
2025-04-06 19:03:31 -0300

    Proyecto mvn creado

---
+++ src/main/java/com/example/validadorcorrelativaspracticadds2025/ValidadorCorrelativasPracticaDds2025Application.java
@@ -0,0 +1,13 @@
+package com.example.validadorcorrelativaspracticadds2025;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValidadorCorrelativasPracticaDds2025Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ValidadorCorrelativasPracticaDds2025Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/validadorcorrelativaspracticadds2025/ValidadorCorrelativasPracticaDds2025ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.validadorcorrelativaspracticadds2025;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValidadorCorrelativasPracticaDds2025ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
